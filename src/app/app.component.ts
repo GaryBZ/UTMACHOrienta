803b4ea3b488901e71aeb249d2659cc9e247c8eb
GaryBZ <gbarreiro2@utmachala.edu.ec>
2026-04-29 16:19:03 -0500

    Arquitectura basica hecha; Navbar, Home y Footer hecho

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  styleUrls: ['app.component.scss'],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
 })
 export class AppComponent {
   constructor() {}
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,11 +3,16 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'home',
-    loadComponent: () => import('./home/home.page').then((m) => m.HomePage),
+    loadComponent: () => import('./pages/home/home.component').then((m) => m.HomeComponent),
+  },
+  {
+    path: 'prueba',
+    loadComponent: () => import('./pages/prueba/prueba.component').then((m) => m.PruebaComponent),
   },
   {
     path: '',
     redirectTo: 'home',
     pathMatch: 'full',
   },
+  
 ];
