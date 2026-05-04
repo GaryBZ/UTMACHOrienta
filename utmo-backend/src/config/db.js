f4f9212d8a470d01d47f7072544a93683a167da4
GaryBZ <gbarreiro2@utmachala.edu.ec>
2026-05-04 17:28:27 -0500

    Ignorar node_modules y .env del backend

---
+++ utmo-backend/src/config/db.js
@@ -0,0 +1,17 @@
+import { Pool, types } from 'pg';
+import dotenv from 'dotenv';
+dotenv.config();
+
+types.setTypeParser(1700, (val) => parseFloat(val));
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
+  ssl: { rejectUnauthorized: false }
+});
+
+
+export default pool;
