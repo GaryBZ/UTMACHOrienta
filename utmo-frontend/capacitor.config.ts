68af1e9831cbab9d219178859b29ad6f9edb24d7
Javier-15-03 <javiercellan8@gmail.com>
2026-05-07 21:33:42 -0500

    feat: agregar splash screen y configuracion de Android con Capacitor

--- utmo-frontend/capacitor.config.ts
+++ utmo-frontend/capacitor.config.ts
@@ -3,7 +3,15 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'UTMACHOrienta',
-  webDir: 'www'
+  webDir: 'www',
+  plugins: {
+  SplashScreen: {
+    launchShowDuration: 3000,
+    backgroundColor: "#ffffff",
+    showSpinner: false
+  }
+}
 };
+
 
 export default config;
